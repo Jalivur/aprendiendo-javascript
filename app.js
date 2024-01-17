5e3b1f75b21060247a258b10ebf5d85bdc80c333
Alberto Estella Quel <147421452+Jalivur@users.noreply.github.com>
2024-01-17 05:30:31 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("hola mundo")
+let respuesta; 
+respuesta = prompt("Como te llamas:");
+let numero1 = parseInt(prompt("dime un numero"),10);
+let numero2 = parseInt(prompt("dime otro numero"),10);
+
+function suma(numero1, numero2){
+    return numero1 + numero2;
+}
+
+document.write(`<p id="parrafo1">Hola <b>${respuesta}</b>: <br>
+la suma de tus numeros (${numero1}, ${numero2}) es igual 
+${suma(numero1 , numero2 )} <br></p>`)
+
+function factorial(n) {
+    if (n === 0 || n === 1) return 1;
+    else return n * factorial(n - 1);
+  }
+
+document.write(`<p class="factorial">El factorial de ${numero1} es <b>${factorial(numero1)}</b> <br></p>`);
+document.write(`<p class="factorial">El factorial de ${numero2} es <b>${factorial(numero2)}</b></p>`);
