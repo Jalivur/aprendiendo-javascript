9bc6fe19e15b9ddf40a89b51abca7f9006e01a11
Alberto Estella Quel <147421452+Jalivur@users.noreply.github.com>
2024-01-18 03:33:29 +0100

    Actualizacion archivos

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ function update(){
     let numero1 = parseInt(numero1imput.value,10);
     let numero2input = document.getElementById("number2");
     let numero2 = parseInt(numero2input.value,10);
-    let resultP = document.getElementById("result1")
+    let resultP1 = document.getElementById("result1")
     let result1;
+    let resultP2 = document.getElementById("result2");
     let result2;
+    let resultP3 = document.getElementById("result3");
     let result3;
 
     function suma(numero1, numero2){
@@ -16,13 +18,15 @@ function update(){
     result1 = `<p id="parrafo1">Hola <b>${respuesta}</b>: <br>
     la suma de tus numeros (${numero1}, ${numero2}) es igual 
     ${suma(numero1 , numero2 )} <br></p>`;
-    resultP.innerHTML = result1
+    resultP1.innerHTML = result1
     function factorial(n) {
         if (n === 0 || n === 1) return 1;
         else return n * factorial(n - 1);
     }
 
-    //document.write("result",`<p class="factorial">El factorial de ${numero1} es <b>${factorial(numero1)}</b> <br></p>`);
-    //document.write("result",`<p class="factorial">El FACTORIAL de ${numero2} es <b>${factorial(numero2)}</b></p>`);
+    result2= `<p class="factorial">El factorial de ${numero1} es <b>${factorial(numero1)}</b> <br></p>`;
+    resultP2.innerHTML = result2
+    result3= `<p class="factorial">El factorial de ${numero2} es <b>${factorial(numero2)}</b></p>`;
+    resultP3.innerHTML = result3
 
 }
