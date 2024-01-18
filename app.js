8e7893bb71c25f35ce1677ad70c9b45f8f3a0936
Alberto Estella Quel <147421452+Jalivur@users.noreply.github.com>
2024-01-18 03:31:32 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
-let respuesta; 
-respuesta = prompt("Como te llamas:");
-let numero1 = parseInt(prompt("dime un numero"),10);
-let numero2 = parseInt(prompt("dime otro numero"),10);
+function update(){
+    let respuestainput = document.getElementById("name");
+    let respuesta = respuestainput.value;
+    let numero1imput = document.getElementById("number1");
+    let numero1 = parseInt(numero1imput.value,10);
+    let numero2input = document.getElementById("number2");
+    let numero2 = parseInt(numero2input.value,10);
+    let resultP1 = document.getElementById("result1")
+    let result1;
+    let resultP2 = document.getElementById("result2");
+    let result2;
+    let resultP3 = document.getElementById("result3");
+    let result3;
 
-function suma(numero1, numero2){
-    return numero1 + numero2;
+    function suma(numero1, numero2){
+        return numero1 + numero2;
+        }
+    result1 = `<p id="parrafo1">Hola <b>${respuesta}</b>: <br>
+    la suma de tus numeros (${numero1}, ${numero2}) es igual 
+    ${suma(numero1 , numero2 )} <br></p>`;
+    resultP1.innerHTML = result1
+    function factorial(n) {
+        if (n === 0 || n === 1) return 1;
+        else return n * factorial(n - 1);
+    }
+
+    result2= `<p class="factorial">El factorial de ${numero1} es <b>${factorial(numero1)}</b> <br></p>`;
+    resultP2.innerHTML = result2
+    result3= `<p class="factorial">El factorial de ${numero2} es <b>${factorial(numero2)}</b></p>`;
+    resultP3.innerHTML = result3
+
 }
-
-document.write(`<p id="parrafo1">Hola <b>${respuesta}</b>: <br>
-la suma de tus numeros (${numero1}, ${numero2}) es igual 
-${suma(numero1 , numero2 )} <br></p>`)
-
-function factorial(n) {
-    if (n === 0 || n === 1) return 1;
-    else return n * factorial(n - 1);
-  }
-
-document.write(`<p class="factorial">El factorial de ${numero1} es <b>${factorial(numero1)}</b> <br></p>`);
-document.write(`<p class="factorial">El FACTORIAL de ${numero2} es <b>${factorial(numero2)}</b></p>`);
